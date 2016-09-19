bd6ae603ea4323c41a73d41eb3c8d8e0a4fc29d9
Prudhvi <prudhvi.moturi@vensaiinc.com>
2016-09-19 11:29:36 +0530

    code migrated to swift 3.0

---
+++ CoxAxle/SelectionTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SelectionTableViewCell.swift
+//  CoxAxle
+//
+//  Created by Prudhvi on 12/09/16.
+//  Copyright © 2016 Prudhvi. All rights reserved.
+//
+
+import UIKit
+
+class SelectionTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
